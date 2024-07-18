bfb6b5425cc09c3e0c3d1dfc34fafec1e43f9a01
Alexanie <ocxigin@gmail.com>
2024-07-18 16:02:45 +0100

    Add two new files catering & order.html

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 //Static Files
 app.use(express.static("public"));
-// app.use(express.static("js", express.static(__dirname + "public/js")));
+app.use(express.static("css", express.static(__dirname + "public/css")));
 
-app.get("", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/layouts/catering", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "layouts", "catering.html"));
+});
+
+app.get("/layouts/order", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "layouts", "order.html"));
 });
 
 app.listen(PORT, () => {
